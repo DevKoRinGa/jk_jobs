57d17a148af1c0edc8eea2e79d50efd62404f468
KoRinGa <42917670+DevKoRinGa@users.noreply.github.com>
2019-04-27 15:16:51 -0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,32 +1,32 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('esx_empregos:setJobt')
-AddEventHandler('esx_empregos:setJobt', function(job)
+RegisterServerEvent('esx_jk_jobs:setJobt')
+AddEventHandler('esx_jk_jobs:setJobt', function(job)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.setJob("taxi", 0)-- 0 is job grade
 	TriggerClientEvent('esx:showNotification', source, 'You were hired ~g~' .. xPlayer.getName())
 end)
 
-RegisterServerEvent('esx_empregos:setJobm')
-AddEventHandler('esx_empregos:setJobm', function(job)
+RegisterServerEvent('esx_jk_jobs:setJobm')
+AddEventHandler('esx_jk_jobs:setJobm', function(job)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.setJob("mechanic", 0)-- 0 is job grade
 	TriggerClientEvent('esx:showNotification', source, 'You were hired ~g~' .. xPlayer.getName())
 end)
 
-RegisterServerEvent('esx_empregos:setJobp')
-AddEventHandler('esx_empregos:setJobp', function(job)
+RegisterServerEvent('esx_jk_jobs:setJobp')
+AddEventHandler('esx_jk_jobs:setJobp', function(job)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.setJob("fisherman", 0)-- 0 is job grade
 	TriggerClientEvent('esx:showNotification', source, 'You were hired ~g~' .. xPlayer.getName())
 end)
 
-RegisterServerEvent('esx_empregos:setJobn')
-AddEventHandler('esx_empregos:setJobn', function(job)
+RegisterServerEvent('esx_jk_jobs:setJobn')
+AddEventHandler('esx_jk_jobs:setJobn', function(job)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	xPlayer.setJob("miner", 0) -- 0 is job grade
