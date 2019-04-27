96d22476ea46e276cf81387237a744e9463033e1
KoRinGa <42917670+DevKoRinGa@users.noreply.github.com>
2019-04-27 14:43:28 -0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -4,19 +4,18 @@ dependency 'es_extended'
 ui_page "html/ui.html"
 
 files {
-	"html/ui.html",
+  "html/ui.html",
   "html/js/index.js",
-  "html/css/style.css",
+  "html/css/style.css"
 }
 
 client_script {
   '@es_extended/locale.lua',
   'config.lua',
-  'client.lua',
+  'client.lua'
 }
 
 server_script {
   '@es_extended/locale.lua',
-  'config.lua',
   'server.lua'
 }
